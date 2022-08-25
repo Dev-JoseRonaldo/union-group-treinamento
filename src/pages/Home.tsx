c356ebe168022a9c6ca8625fe35b4bdd56167817
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-25 17:54:47 -0300

    :sparkles: feat: Criação e estilização do component Button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom"
 
+import styles from '../styles/home.module.scss'
+
 export const Home = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/reactjs">React.js</Link></li>
-          <li><Link to="/typescript">Typescript</Link></li>
-          <li><Link to="/sass">Sass</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <main className={styles.container}>
+        <nav>
+          <ul>
+            <li><Link to="/reactjs">React.js</Link></li>
+            <li><Link to="/typescript">Typescript</Link></li>
+            <li><Link to="/sass">Sass</Link></li>
+          </ul>
+        </nav>
+      </main>
+    </>
   )
 }
--- src/pages/Typescript.tsx
+++ src/pages/Typescript.tsx
@@ -1,10 +1,59 @@
 import { Link } from "react-router-dom"
+import { Button } from "../components/Button"
+
+import styles from '../styles/typescript.module.scss'
 
 export const Typescript = () => {
+  function handleClick(name: string, age: number) {
+    console.log(`Me chama ${name} e tenho ${age} anos`)
+  }
+
   return (
     <>
-      <h1>Página do Typescript</h1>
-      <Link to="/">Voltar</Link>
+      <main>
+        <Link to="/">Voltar</Link>
+        <h1>React com Typescript</h1>
+        <section className={styles.buttons}>
+          <h2>Uso do Component Button</h2>
+          {/* Utiliza o valor padrão das props */}
+          <Button onClick={() => handleClick('Leandro Lopes', 27)}>Button</Button>
+
+          {/* variant secondary */}
+          <Button
+            variant="secondary"
+            onClick={() => handleClick('Leandro Lopes', 27)}
+          >
+            Button
+          </Button>
+
+          {/* variant outline */}
+          <Button
+            variant="outline"
+            onClick={() => handleClick('Leandro Lopes', 27)}
+          >
+            Button
+          </Button>
+
+          {/* size small */}
+          <Button size="small" onClick={() => handleClick('Leandro Lopes', 27)}>
+            Button
+          </Button>
+
+          {/* size medium */}
+          <Button
+            variant="secondary"
+            size="small"
+            onClick={() => handleClick('Leandro Lopes', 27)}
+          >
+            Button
+          </Button>
+
+          {/* full width */}
+          <Button fullWidth onClick={() => handleClick('Leandro Lopes', 27)}>
+            Button
+          </Button>
+        </section>
+      </main>
     </>
   )
 }
